9b061f7ae3bc38620bed3934fc7e71001e5768e5
vivianu2001 <118671563+vivianu2001@users.noreply.github.com>
2022-11-20 20:16:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,20 +4,19 @@ int main()
 {
 	int num1,num2=0;
 	int temp=0;
-	printf("please enter a numbers:\n");
 	scanf("%d%d",&num1,&num2);
 
 if(num1>num2)
 {
 temp= num1;
 num1=num2;
-num2=num1;
+num2=temp;
 }
 
 
 	while (num1<0|| num2<0)
 	{
-		printf("please enter a valid numbers:\n");
+		
 		scanf("%d%d",&num1,&num2);
 	
     }
@@ -26,24 +25,11 @@ num2=num1;
         {
          temp= num1;
          num1=num2;
-         num2=num1;
+         num2=temp;
          }
-
-	printf("The Prime numbers between %d,%d :",num1,num2);
-
-
-	for (int i = num1; i <=num2; i++)
-	{
-		if(isPrime(i))
-		{
-			printf("  %d",i);
-		}
-		
-	}
-
-	printf("\n");
-
-    printf("The Armstrong numbers between %d,%d :",num1,num2);
+	
+	
+    printf("The Armstrong numbers are:");
 
     for (int j = num1; j<=num2; j++)
 	{
@@ -55,35 +41,53 @@ num2=num1;
 	}
 
 	   printf("\n"); 
-
-	   
-
-	  printf("The strong numbers between %d,%d :",num1,num2);
-
-
-      for (int i = num1; i <=num2; i++)
-	 {
-		if (isStrong(i))
-		{
-			printf("  %d",i);
-		}
-	 }
-
-		printf("\n");
-
-
-	 printf("The polindrome numbers between %d,%d :",num1,num2);
+	
+	 printf("The Palindromes are:");
 
 
       for (int i = num1; i <=num2; i++)
 	  {
 		if (isPalindrome(i))
 		{
-			printf("  %d",i);
+			printf(" %d",i);
 		}
 	   }
 
 	   printf("\n"); 
+
+	
+	
+
+	printf("The Prime numbers are:");
+
+
+	for (int i = num1; i <=num2; i++)
+	{
+		if(isPrime(i))
+		{
+			printf(" %d",i);
+		}
+		
+	}
+
+	printf("\n");
+
+
+	  printf("The Strong numbers are:");
+
+
+      for (int i = num1; i <=num2; i++)
+	 {
+		if (isStrong(i))
+		{
+			printf(" %d",i);
+		}
+	 }
+
+		printf("\n");
+
+
+
 
 	  return 0;
 
